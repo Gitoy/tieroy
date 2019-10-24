fd93368b1d193ef4c6c7953aafe65a9bb7c76b49
Roy Stroek <r.l.stroek@gmail.com>
2019-10-24 22:04:20 +0200

    removed settings

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -12,7 +12,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://nl.wikipedia.org/wiki/Hoofdpagina"
           target="_blank"
           rel="noopener noreferrer"
         >
